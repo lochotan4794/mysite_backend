d96359de3f4cb064c2f83b731df395395bbf173f
lochotan4794 <htanloc994@gmail.com>
2022-08-09 12:48:31 +0700

    change be

--- registration/views.py
+++ registration/views.py
@@ -55,6 +55,35 @@ def index(request):
     return HttpResponse('Please confirm your email address to complete the registration')
 
 
+@csrf_exempt
+def verify_email(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            # save form in the memory not in database
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            # to get the domain of the current site
+            current_site = get_current_site(request)
+            mail_subject = 'Activation link has been sent to your email id'
+            message = render_to_string('registration/acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
--- users/serializers.py
+++ users/serializers.py
@@ -15,4 +15,5 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', "first_name", "last_name", 'profile']
+        fields = ['username', 'email', "first_name",
+                  "last_name", 'profile', 'is_active']
