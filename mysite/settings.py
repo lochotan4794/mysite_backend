2680bd7eddbee1ebae4f8f7a4b0ee7c3fb0f57dc
loc <htanloc994@gmail.com>
2022-10-08 09:55:49 +0700

    change setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -179,14 +179,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 #     },
 # }
 
-if 'RDS_DB_NAME' in os.environ:
+if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
+            'HOST': os.environ['c'],
             'PORT': os.environ['RDS_PORT'],
         }
     }
