e097bdb6e23d831ace78edc48983dfac143e2711
lochotan4794 <49067965+lochotan4794@users.noreply.github.com>
2025-05-19 21:14:44 +0700

    fix twillio 123

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,6 +19,10 @@ from django.utils.encoding import force_str
 django.utils.encoding.force_text = force_str
 import six
 from urllib.parse import urlsplit, urlunsplit, unquote
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
